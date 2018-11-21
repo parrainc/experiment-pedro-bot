b8aa6233b2a85fbc2d571affd6dc0966fbae9091
Carlos Parra <carlos.parra.sbr@gmail.com>
2018-11-20 22:29:39 -0400

    Adding mini server for pedro's bot

---
+++ app.js
@@ -0,0 +1,41 @@
+"use strict";
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+const axios = require('axios');
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+app.post('/new-message', function(req, res) {
+    const { message } = req.body;
+
+    if (!message || message.text.toLowerCase().indexOf('pedro') < 0) {
+        return res.end();
+    }
+
+    axios
+        .post(
+            'https://api.telegram.org/[PEDRO_BOT_API_KEY]/sendMessage', 
+            {
+                chat_id: message.chat.id,
+                text: 'Hola, Soy Pedro'
+            }
+        )
+        .then(response => {
+            console.log('Message posted : ', response);
+            res.end('ok');
+        })
+        .catch(err => {
+            console.log('Error : ' + err);
+            res.end('Error : ' + err);
+        });
+});
+
+app.listen(3000, function() {
+    console.log("Pedro bot's listening on port 3000!");
+});
