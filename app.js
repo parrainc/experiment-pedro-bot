00d5cec9d65547d803bcbfc32bd7db383326ce9e
Carlos Parra <carlos.parra.sbr@gmail.com>
2018-11-21 00:01:02 -0400

    Adding basic list of commands and doing some changes when caught the request

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 const axios = require('axios');
+const listOfCommands = [
+    'dialgo',
+    'pizza',
+    'relaja',
+    'comparte',
+    'opina',
+    'ataca',
+    'rh'
+];
 
 app.use(bodyParser.json());
 app.use(
@@ -12,35 +21,48 @@ app.use(
 );
 
 app.get('/', function(req, resp) {
-    console.log('This is Pedro bot home pages');
+    console.log(`This is Pedro's bot home page`);
     resp.end('nothing to do here');
 });
 
 app.post('/new-message', function(req, res) {
     const { message } = req.body;
 
-    if (!message || message.text.toLowerCase().indexOf('pedro') < 0) {
-        return res.end();
+    if (!message) {
+        return res.status(400).end('payload is undefined!');
+    }
+
+    if (listOfCommands.filter(command => command.endsWith(message.text.toLowerCase())) < 1) {
+        axios
+            .post(
+                `https://api.telegram.org/${process.env.PEDRO_BOT_API_KEY}/sendMessage`,
+                {
+                    chat_id: message.chat.id,
+                    text: `Uhm... I'm not enough smart to determine what do you mean by that. 
+                    Try another command :)`
+                }
+            );
+        return res.status(404).end('command not found');
     }
 
     axios
         .post(
-            'https://api.telegram.org/'+ process.env.PEDRO_BOT_API_KEY+'/sendMessage', 
+            `https://api.telegram.org/${process.env.PEDRO_BOT_API_KEY}/sendMessage`,
             {
                 chat_id: message.chat.id,
-                text: 'Hola, Soy Pedro'
+                text: 'Hey, this is pedro. ' + message.text
             }
         )
         .then(response => {
             console.log('Message posted : ', response);
-            res.end('ok');
+            res.status(200).end('ok');
         })
         .catch(err => {
             console.log('Error : ' + err);
-            res.end('Error : ' + err);
+            res.status(500).end('Error : ' + err);
         });
 });
 
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Pedro bot's listening on port 3000!");
+    console.log("Pedro's bot is listening! Have fun.");
 });
